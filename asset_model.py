55302481e11966a920db020347beaafebcc967a0
Mattia De Tommaso <91329796+leone2497@users.noreply.github.com>
2024-09-24 13:36:53 +0200

    Update asset_model.py

--- asset_model.py
+++ asset_model.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+import easyocr
+import numpy as np
+from glob import glob 
+from PIL import Image
+
+st.title("Modello forecast degli assetti di centrale to be")
+st.sidebar.title("Functions")
+file_to_analyze = st.file_uploader("Choose a CSV or Excel file", type=["csv", "xls", "xlsx"])
+
+if file_to_analyze is not None:
+  if file_to_analyze.name.endswith('.csv'):
+  df = pd.read_csv(file_to_analyze)
+  else:
+    df = pd.read_excel(file_to_analyze)
 
