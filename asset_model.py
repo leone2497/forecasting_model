79dcf2f7867f9265afd64404d49cd98f24f1aeed
Mattia De Tommaso <91329796+leone2497@users.noreply.github.com>
2024-09-28 23:12:29 +0200

    Update asset_model.py

--- asset_model.py
+++ asset_model.py
@@ -161,7 +161,7 @@ if dataframes:
             # Calculate 'Rapporto potenza assorbita/pot tot' if applicable (based on 2nd and 3rd columns)
             if merged_df.shape[1] >= 3:
                 merged_df["Rapporto potenza assorbita/pot tot"] = merged_df.iloc[:, 1] / merged_df.iloc[:, 2]
-                merged_df["Fuel/Rapporto potenza assorbita"] = merged_df.iloc[:, 3] / merged_df.iloc[:, 1]
+                merged_df["Fuel/Rapporto potenza assorbita"] = (merged_df.iloc[:, 3]*1000) / merged_df.iloc[:, 1]
 
                 # Create classes for 'Rapporto potenza assorbita/pot tot'
                 merged_df['Class'] = pd.cut(
