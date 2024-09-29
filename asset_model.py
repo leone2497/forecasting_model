64de13433d0b0bbdf220f1cf04c28e46d3e10157
Mattia De Tommaso <91329796+leone2497@users.noreply.github.com>
2024-09-29 12:58:27 +0200

    Update asset_model.py

--- asset_model.py
+++ asset_model.py
@@ -184,4 +184,5 @@ if df is not None:
 
         # Option to download the DataFrame with assigned machines
         assigned_machine_csv = df.to_csv(index=False)
-        st.download_button(label
+        st.download_button(label="Download Data with Assigned Machines",
+                           data=assigned_machine
